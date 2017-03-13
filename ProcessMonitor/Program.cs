773a48abaf4674e09a2b3c89b73de6a4159276f0
Michael Lipscombe <mlipscombe@outlook.com>
2017-03-13 11:09:52 -0400

    handle exit code and msg completion

--- ProcessMonitor/Program.cs
+++ ProcessMonitor/Program.cs
@@ -4,53 +4,74 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProcessMonitor
 {
     class Program
     {
-        const int BYTESINMEG = 1000000;
-        private static JobObject jobObject;
-        private static Process process;
+        const int BYTES_IN_MEG = 1000000;
+
+        private static JobObject _jobObject;
+        private static Process _process;
+
         static void Main(string[] args)
         {
             // start hwc
             var startInfo = new ProcessStartInfo(@"C:\containerizer\hwc.exe", @"-appRootPath env\WebApiMemoryLimit");
             startInfo.WorkingDirectory = @"C:\containerizer\";            
             Environment.SetEnvironmentVariable("PORT", "8081");
-            process = Process.Start(startInfo);
+            _process = Process.Start(startInfo);
+            _process.EnableRaisingEvents = true;
 
             // try to capture process exit code on failure
-            process.Exited += Process_Exited;
+            _process.Exited += Process_Exited;
 
             // assign process to JO
-            jobObject = new JobObject(null);            
-            jobObject.AssignProcessToJob(process.Handle);
+            _jobObject = new JobObject(null);            
+            _jobObject.AssignProcessToJob(_process.Handle);
+
+   
+            Task.Run(() =>
+            {
+                //var currentStatus = JobObject.CompletionMsg.NoCompletionStatus;
+                //while (currentStatus!= JobObject.CompletionMsg.ActiveProcessZero)
+                //{
+                    foreach (var msg in _jobObject.GetQueuedCompletionStatus())
+                    {
+                        Console.WriteLine($"Completion Message:{msg.ToString()}");
+                       // currentStatus = msg;
+                    }
+
+                //}
+
+                Console.WriteLine("Process no longer exists!");
+            });
             
             // set limits 
-            JobObjectLimits limits = new JobObjectLimits(jobObject, new TimeSpan(0, 0, 10));
-            limits.LimitMemory(256 * BYTESINMEG);
+            JobObjectLimits limits = new JobObjectLimits(_jobObject, new TimeSpan(0, 0, 10));
+            limits.LimitMemory(256 * BYTES_IN_MEG);
             limits.MemoryLimitReached += Limits_MemoryLimitReached;
-            Console.WriteLine($"Memory Limit Set:{jobObject.GetJobMemoryLimit()/ BYTESINMEG}");
+            Console.WriteLine($"Memory Limit Set:{_jobObject.GetJobMemoryLimit()/ BYTES_IN_MEG} MB");
 
             // monitor memory peak 
             while(true)
             {
-                Console.WriteLine($"Job Peak Memory: {jobObject.GetPeakJobMemoryUsed()/ BYTESINMEG}");
-                System.Threading.Thread.Sleep(5 * 1000);
+                Console.WriteLine($"Job Peak Memory: {_jobObject.GetPeakJobMemoryUsed()/ BYTES_IN_MEG} MB");
+                Thread.Sleep(5 * 1000);
             }
         }
 
         private static void Process_Exited(object sender, EventArgs e)
         {
-            Console.WriteLine($"hwc exit code {process.ExitCode}");
+            Console.WriteLine($"hwc exit code {_process.ExitCode}");
         }
 
         private static void Limits_MemoryLimitReached(object sender, EventArgs e)
         {
-            Console.Error.WriteLine("Memory Limit Reached!");
-            jobObject.TerminateProcessesAndWait(1000);
+            Console.Error.WriteLine("Memory Limit Reached! Terminating Job...");
+           // _jobObject.TerminateProcessesAndWait();
             Console.Error.WriteLine("Process terminated");
         }
     }
